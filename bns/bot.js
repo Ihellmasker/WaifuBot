b10e2bfe99c64d3c2ad8dde5f0e59ef04f5a6ca0
Andy Taylor <ihm.andrew@gmail.com>
2016-01-28 14:17:02 +0000

    Added region joining stuff and remove beta functions

--- bns/bot.js
+++ bns/bot.js
@@ -8,9 +8,18 @@ var botcore = require('./../lib/botcore.js');
 var streamIntTimer;
 
 var _commands = {
-	"beta": {
-		"function": betaTimer
-	},
+    "joineu": {
+        "function": joinEU
+    },
+    "joinna": {
+        "function": joinNA
+    },
+    "leaveeu": {
+        "function": leaveEU
+    },
+    "leavena": {
+        "function": leaveNA
+    },
 	"installer": {
 		"function": installerLink
 	},
@@ -18,66 +27,33 @@ var _commands = {
 		"aliasOf": "installer"
 	}
 };
-var betaDates = [
-	{
-		"date": "24-11-2015 10:00 -0800",
-		"type": "start",
-		"event": "Beta 3"
-	},
-	{
-		"date": "30-11-2015 22:00 -0800",
-		"type": "end",
-		"event": "Beta 3"
-	},
-	{
-		"date": "11-12-2015 10:00 -0800",
-		"type": "start",
-		"event": "Beta 4"
-	},
-	{
-		"date": "14-12-2015 22:00 -0800",
-		"type": "end",
-		"event": "Beta 4"
-	},
-	{
-		"date": "18-12-2015 10:00 -0800",
-		"type": "start",
-		"event": "Beta 5"
-	},
-	{
-		"date": "21-12-2015 22:00 -0800",
-		"type": "end",
-		"event": "Beta 5"
-	}
-];
 
 botcore.login(_commands, auth, "!");
 
-function betaTimer(params, message) {
-	if (botcore.accepting()) {
-		var todaydate = new moment();
-		var releasedate = new moment();
-		var difference = -1;
-		var current = 0;
-		while (difference < 0 && current < betaDates.length) {
-			releasedate = new moment(betaDates[current].date, "DD-MM-YYYY HH:mm Z");;
-			difference = Math.floor((releasedate.unix() - todaydate.unix()));
-			if (difference > 86400) { // Over a day
-				var days = Math.floor(difference / (24 * 60 * 60));
-				botcore.bot().sendMessage(message.channel, betaDates[current].event + " will " + betaDates[current].type + " in over " + days + " day" + (days > 1 ? "s" : ""));
-			} else if (difference > 3600) { // Over an hour
-				var hours = Math.floor(difference / (60 * 60));
-				botcore.bot().sendMessage(message.channel, betaDates[current].event + " will " + betaDates[current].type + " in over " + hours + " hour" + (hours > 1 ? "s" : ""));
-			} else if (difference > 0){ // MINUTES
-				var minutes = Math.floor(difference / 60);
-				botcore.bot().sendMessage(message.channel, betaDates[current].event + " will " + betaDates[current].type + " in " + minutes + " minute" + (minutes > 1 ? "s" : ""));
-			}
-			current++;
-		}
-	}
-}
 function installerLink(params, message) {
 	if (botcore.accepting()) {
-		botcore.bot().sendMessage(message.channel, "You can download the game at http://cbt.patcher.bladeandsoul.com/BnS_CBT/installer/BnS_CBT%20Lite%20Installer.exe");
+	    botcore.bot().sendMessage(message.channel, "You can download the game at http://www.bladeandsoul.com/en/download/");
 	}
+}
+function joinEU(params, message) {
+    joinregion(params, message, "EU");
+}
+function joinNA(params, message) {
+    joinregion(params, message, "NA");
+}
+function joinregion(params, message, region) {
+    if (!message.channel.isPrivate) {
+        botcore.bot().addMemberToRole(message.author, message.channel.server.roles.get("name", region));
+    }
+}
+function leaveEU(params, message) {
+    leaveregion(params, message, "EU");
+}
+function leaveNA(params, message) {
+    leaveregion(params, message, "NA");
+}
+function leaveregion(params, message, region) {
+    if (!message.channel.isPrivate) {
+        botcore.bot().removeMemberFromRole(message.author, message.channel.server.roles.get("name", region));
+    }
 }
